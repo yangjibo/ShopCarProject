c90ada1552c770d6795e038272928707c54758ab
Administrator <18865503122@163.com>
2017-03-21 09:44:16 +0800

    ShopCar

---
+++ app/src/main/java/com/maitai/www/shopcarproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.maitai.www.shopcarproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
